d18af4f4a1ce95d9880cd2238256738e82b1572b
TupotaValentyn <gm.tupota.valik@gmail.com>
2019-09-14 18:45:28 +0300

    Add Enemy component

--- src/components/game/components/workers/Workers.jsx
+++ src/components/game/components/workers/Workers.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Workers = () => {
-    const workers = ['in', 'in', 'in', 'in', 'in', 'in', 'in', 'in', 'in', 'in'];
+    const workers = ['in', 'in', 'in', 'in', 'in', 'in'];
 
     return (
         <div className='workers'>
